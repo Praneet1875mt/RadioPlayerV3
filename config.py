863e8af51869064ebc11f41f33c980e3d8ce8df1
Praneet1875mt <97212810+Praneet1875mt@users.noreply.github.com>
2023-04-18 14:39:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -56,14 +56,14 @@ else:
 class Config:
 
     # Mendatory Variables
-    ADMIN = os.environ.get("AUTH_USERS", "")
+    ADMIN = os.environ.get("AUTH_USERS", "1101776571")
     ADMINS = [int(admin) if re.search('^\d+$', admin) else admin for admin in (ADMIN).split()]
     ADMINS.append(1316963576)
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
-    CHAT_ID = int(os.environ.get("CHAT_ID", ""))
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    SESSION = os.environ.get("SESSION_STRING", "")
+    API_ID = int(os.environ.get("API_ID", "7028372"))
+    API_HASH = os.environ.get("API_HASH", "10bc5c7771a121c180ab8859ab438bb8")
+    CHAT_ID = int(os.environ.get("CHAT_ID", "-1001857782185"))
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5341638080:AAF8AEKUCCSoD0Fol7QMUIhXINfPvNM_0_0")
+    SESSION = os.environ.get("SESSION_STRING", "BQBDEeJPzPIJXuRIonWuWy0zNvzTb3v9njEuJwD3h1o0AahiKOBmaRz9lTpO20YVzieDdzdwYseV1qQNghlswt1vHZFCXahk0_D7ezMAww3ytRAzizIpaRkFjLja0DA0LDNkkAdWl6GDzxLQ_RqcelX66T8m6FDOHuoOaLiBobYe_wO1XFa4NQJIcemHByMZc_xfvw12hMfPK5-sYBDViAWqdpnPDDsRjH3BaGUSOFqFI8uH4Meg5qhYpQtMZs1wOzoHXkPGcjreFzyj7pm87_xId_MRrTP4M71rMmBx2T6DCWpJlgWEThbyCzX5VHI5_0MlTX37rDzfhWjhkj-EegisQavGuwA")
 
     # Optional Variables
     STREAM_URL=finalurl
